aed17a3192fb7c853fa5301058b30d5c03038127
Juan José Reyes Vilchis <maylo360xd@gmail.com>
2020-06-08 02:19:24 -0500

    Update RSA.py

--- Utils/RSA.py
+++ Utils/RSA.py
@@ -5,7 +5,7 @@ from Crypto.Hash import SHA512, SHA384, SHA256, SHA, MD5
 from Crypto import Random
 from base64 import b64encode, b64decode
 
-hash = "SHA-256"
+hash = "SHA-512"
 
 def newkeys(keysize):
     random_generator = Random.new().read
